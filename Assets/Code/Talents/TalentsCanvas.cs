e88939b20b305f7c395023d1540d91290c46417b
Егор Агеев <82820305+Ryagich@users.noreply.github.com>
2024-04-13 12:38:39 +0500

    change talents canvas

--- Assets/Code/Talents/TalentsCanvas.cs
+++ Assets/Code/Talents/TalentsCanvas.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 using System.Collections.Generic;
 using UnityEngine.Serialization;
@@ -12,16 +13,20 @@ public class TalentsCanvas : MonoBehaviour
     [field: SerializeField] public TMP_Text Description { get; private set; }
 
     [SerializeField] private GameObject _canvas;
+    [SerializeField] private GameObject _card;
+    [SerializeField] private Image _icon;
     [SerializeField] private SelectedTalent _selectedTalentPref;
 
-    private List<SelectedTalent> selectedTalents = new();
+    [SerializeField] private List<SelectedTalent> selectedTalents = new();
     private void Awake()
     {
+        _card.gameObject.SetActive(false);
         Instance = this;
     }
 
     public void SetUp()
     {
+        Clear();
         var selectedTalentsInfo = TalentsController.Instance.GetSelectedTalentsInfo();
 
         foreach (var info in selectedTalentsInfo)
@@ -36,12 +41,15 @@ public class TalentsCanvas : MonoBehaviour
 
     private void UpdateDescription(TalentInfo info)
     {
+        _card.gameObject.SetActive(true);
+        _icon.sprite = info.Sprite;
         Name.text = info.Name;
         Description.text = info.Description;
     }
 
     public void Clear()
     {
+        _card.gameObject.SetActive(false);
         Name.text = "";
         Description.text = "";
         foreach (var st in selectedTalents)
